82a04d832bb51f481ccf5897b2a39dc30b3c225d
OrleansParishIT <jadogeri@uno.edu>
2023-11-28 16:25:24 -0600

    added Rock Paper Scissors and Madlib

---
+++ Beginner/Madlib/script.sh
@@ -0,0 +1,20 @@
+# Author : Joseph Adogeri 
+# Date       : 11/28/2023
+# Title      : Basic Subtracting Calculator
+# Descripton : Takes user inputs then generates text 
+
+echo "************** MADLIB ***************"
+sleep(3)
+echo "Enter your name : "
+
+read name
+sleep(3)
+
+echo "Enter birthday"
+
+read birthday
+sleep(3)
+
+let "value = $num1 - $num2"
+
+echo "THE SUM BETWEEN $num1 - $num2 = $value"
